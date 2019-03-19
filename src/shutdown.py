dcf7f9c5ab42ba035dfcc3cebbec0146330f49eb
Erwin van Thiel <vanthiel.erwin@gmail.com>
2019-03-19 18:48:43 +0100

    added system check

--- src/shutdown.py
+++ src/shutdown.py
@@ -1,5 +1,22 @@
 
 def shutdown(delay):
 
-	command = ['shutdown','-h',delay]
-	#o = subprocess.check_output(command, shell=True)
+	if check_platform() == 'Windows':
+		command = ['shutdown','-s','-t',delay]
+	elif check_platform() == 'Linux':
+		command = ['shutdown','-h',delay]
+	elif check_platform() == 'OSX'
+		command = ['']
+		
+	o = subprocess.check_output(command, shell=True)
+
+
+def check_platform():
+	from sys import platform
+	if platform == "linux" or platform == "linux2":
+	    return 'Linux'
+	elif platform == "darwin":
+	    return 'OSX'
+	elif platform == "win32":
+	    return 'Windows'
+
