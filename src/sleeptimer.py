7c20b164008c072f8e60d9c41d2cd32f4bb40793
Erwin van Thiel <vanthiel.erwin@gmail.com>
2019-03-19 14:55:20 +0100

    first commit

---
+++ src/sleeptimer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import subprocess
+import tkinter
+
+class Gui:
+
+	def __init__(self):
+
+		self.root = tkinter.Tk()
+		self.root.geometry("500x300") #You want the size of the app to be 500x500
+		self.root.resizable(0, 0) #Don't allow resizing in the x or y direction
+		self.root.title('Sleeptimer')
+		self.topframe = tkinter.Frame(self.root)
+		self.bottomframe = tkinter.Frame(self.root)
+		self.inputtext = tkinter.Text(self.bottomframe, width=20, height=1) 
+		self.timer = tkinter.Label(self.topframe, text='00:00', fg='black')
+		self.button = tkinter.Button(self.bottomframe, text="start", command=self.start)
+
+
+	def setup_window(self):
+		self.topframe.pack()
+		self.bottomframe.pack()
+		self.inputtext.pack()
+		self.button.pack()
+		self.timer.pack()
+		self.root.mainloop()
+
+	def start(self):
+		try:
+			timervalue = int(self.inputtext.get('1.0'))
+			shutdown(str(timervalue))
+			start_timer()
+		except ValueError:
+			print("not a number")
+			self.inputtext.delete(1.0, tkinter.END)
+ 
+
+
+	
+def start_timer():
+	pass
+
+def shutdown(timer):
+
+	command = ['shutdown','-h',timer]
+	o = subprocess.check_output(command, shell=True)
+
+gui = Gui()
+gui.setup_window()
